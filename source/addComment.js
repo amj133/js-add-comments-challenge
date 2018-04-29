5be59ddb515501a48f6000f5eda84a9a49c34bd0
amj133 <amj@vt.edu>
2018-04-29 15:23:10 -0600

    completed challenge

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-  // add your code here
+  $('#new-comment').hide();
+  $('#new_comment_button').on('click', function(){
+    $('#new_comment_button').hide();
+    $('#new-comment').show();
+  })
+  $('#new-comment').on('submit', function(event){
+    var comment = $( ".new-comment-text" ).val();
+    var author = $( ".new-comment-author" ).val();
+    $(`<li>
+        ${comment}
+        <span class="author">${author}</span>
+       </li>`).appendTo('#comment_list')
+    event.preventDefault();
+    $('#new-comment').hide();
+    $('#new_comment_button').show();
+  })
 })
